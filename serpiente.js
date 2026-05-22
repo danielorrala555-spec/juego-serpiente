2d1003836241289789e2766b7219af4bdbcd7097
danielorrala555-spce <danielorrala555@gmail.com>
2026-05-21 22:59:09 -0500

    add lines to the canvas

--- serpiente.js
+++ serpiente.js
@@ -2,6 +2,12 @@
     // 1. Capturamos el canvas y su contexto de dibujo
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
+    const TAMANIO_CELDA = 25;
+    
+
+
+  ctx.strokeStyle = "#b893934f";
+  ctx.beginPath();
 
 
     
@@ -19,7 +25,21 @@
 
     function dibujarTodo() {
       limpiarCanvas();
+      dibujarTablero();
     }
-
-
-
+    function dibujarTablero(){
+      for (let i = 0; i <= canvas.width ; i+=TAMANIO_CELDA) {
+      ctx.strokeStyle = "#b893934f";
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, canvas.height);
+      ctx.stroke();
+    }
+    for (let i = 0; i <= canvas.height ; i+=TAMANIO_CELDA) {
+      ctx.strokeStyle = "#b893934f";
+      ctx.beginPath();
+      ctx.moveTo(0, i);
+      ctx.lineTo(canvas.width, i);
+      ctx.stroke();
+    }
+}
